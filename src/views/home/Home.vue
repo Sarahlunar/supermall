92eda1d4db7aa9ed39e67334dc54323b257e3c01
sarah <meijuan_zh@163.com>
2020-05-22 13:52:48 +0800

    增加首页nav-bar

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
+    <nav-bar class="home-nav-bar"><div slot="center">购物街</div></nav-bar>
     <h2>我是首页</h2>
   </div>
 </template>
 
 <script>
+  import NavBar from "components/common/navbar/NavBar";
   export default {
-    name: "Home"
+    name: "Home",
+    components: {
+      NavBar
+    }
   }
 </script>
 
 <style scoped>
-
+  .home-nav-bar {
+    background-color: var(--color-tint);
+  }
 </style>
